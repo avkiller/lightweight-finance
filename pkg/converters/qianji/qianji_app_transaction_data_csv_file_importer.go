285af052876630fdd58b16a8ce471864beae0ecd
fireworld <fuxudong103@gmail.com>
2025-12-02 19:56:36 +0800

    优化钱迹APP数据的导入功能

--- pkg/converters/qianji/qianji_app_transaction_data_csv_file_importer.go
+++ pkg/converters/qianji/qianji_app_transaction_data_csv_file_importer.go
@@ -2,7 +2,6 @@ package qianji
 
 import (
 	"bytes"
-	"strings"
 
 	"golang.org/x/text/encoding/unicode"
 	"golang.org/x/text/transform"
@@ -11,8 +10,6 @@ import (
 	"github.com/mayswind/ezbookkeeping/pkg/converters/csv"
 	"github.com/mayswind/ezbookkeeping/pkg/converters/datatable"
 	"github.com/mayswind/ezbookkeeping/pkg/core"
-	"github.com/mayswind/ezbookkeeping/pkg/errs"
-	"github.com/mayswind/ezbookkeeping/pkg/log"
 	"github.com/mayswind/ezbookkeeping/pkg/models"
 )
 
@@ -25,18 +22,19 @@ const qianjiAppTransactionAccountCurrencyColumnName = "币种"
 const qianjiAppTransactionAmountColumnName = "金额"
 const qianjiAppTransactionDescriptionColumnName = "备注"
 const qianjiAppTransactionRelatedIdColumnName = "关联账单"
-const qianjiAppTransactionTypeIncomeText = "收入"
-const qianjiAppTransactionTypeExpenseText = "支出"
+const qianjiAppTransactionTagsColumnName = "标签"
 
 var qianjiAppDataColumnNameMapping = map[datatable.TransactionDataTableColumn]string{
-	datatable.TRANSACTION_DATA_TABLE_TRANSACTION_TIME: qianjiAppTransactionTimeColumnName,
-	datatable.TRANSACTION_DATA_TABLE_TRANSACTION_TYPE: qianjiAppTransactionTypeColumnName,
-	datatable.TRANSACTION_DATA_TABLE_CATEGORY:         qianjiAppTransactionCategoryColumnName,
-	datatable.TRANSACTION_DATA_TABLE_SUB_CATEGORY:     qianjiAppTransactionSubCategoryColumnName,
-	datatable.TRANSACTION_DATA_TABLE_ACCOUNT_NAME:     qianjiAppTransactionAccountNameColumnName,
-	datatable.TRANSACTION_DATA_TABLE_ACCOUNT_CURRENCY: qianjiAppTransactionAccountCurrencyColumnName,
-	datatable.TRANSACTION_DATA_TABLE_AMOUNT:           qianjiAppTransactionAmountColumnName,
-	datatable.TRANSACTION_DATA_TABLE_DESCRIPTION:      qianjiAppTransactionDescriptionColumnName,
+	datatable.TRANSACTION_DATA_TABLE_TRANSACTION_TIME:     qianjiAppTransactionTimeColumnName,
+	datatable.TRANSACTION_DATA_TABLE_TRANSACTION_TYPE:     qianjiAppTransactionTypeColumnName,
+	datatable.TRANSACTION_DATA_TABLE_CATEGORY:             qianjiAppTransactionCategoryColumnName,
+	datatable.TRANSACTION_DATA_TABLE_SUB_CATEGORY:         qianjiAppTransactionSubCategoryColumnName,
+	datatable.TRANSACTION_DATA_TABLE_ACCOUNT_NAME:         qianjiAppTransactionAccountNameColumnName,
+	datatable.TRANSACTION_DATA_TABLE_ACCOUNT_CURRENCY:     qianjiAppTransactionAccountCurrencyColumnName,
+	datatable.TRANSACTION_DATA_TABLE_AMOUNT:               qianjiAppTransactionAmountColumnName,
+	datatable.TRANSACTION_DATA_TABLE_RELATED_ACCOUNT_NAME: qianjiAppTransactionRelatedIdColumnName,
+	datatable.TRANSACTION_DATA_TABLE_TAGS:                 qianjiAppTransactionTagsColumnName,
+	datatable.TRANSACTION_DATA_TABLE_DESCRIPTION:          qianjiAppTransactionDescriptionColumnName,
 }
 
 // qianjiAppTransactionDataCsvFileImporter defines the structure of feidee mymoney app csv importer for transaction data
@@ -52,128 +50,15 @@ func (c *qianjiAppTransactionDataCsvFileImporter) ParseImportedData(ctx core.Con
 	fallback := unicode.UTF8.NewDecoder()
 	reader := transform.NewReader(bytes.NewReader(data), unicode.BOMOverride(fallback))
 
-	csvDataTable, err := csv.CreateNewCsvBasicDataTable(ctx, reader, false)
+	dataTable, err := csv.CreateNewCsvBasicDataTable(ctx, reader, true)
 
 	if err != nil {
 		return nil, nil, nil, nil, nil, nil, err
-	}
-
-	dataTable, err := createNewQianjiAppTransactionBasicDataTable(ctx, csvDataTable)
-
-	if err != nil {
-		return nil, nil, nil, nil, nil, nil, err
-	}
-
-	commonDataTable := datatable.CreateNewCommonDataTableFromBasicDataTable(dataTable)
-
-	if !commonDataTable.HasColumn(qianjiAppTransactionTimeColumnName) ||
-		!commonDataTable.HasColumn(qianjiAppTransactionTypeColumnName) ||
-		!commonDataTable.HasColumn(qianjiAppTransactionSubCategoryColumnName) ||
-		!commonDataTable.HasColumn(qianjiAppTransactionAccountNameColumnName) ||
-		!commonDataTable.HasColumn(qianjiAppTransactionAmountColumnName) ||
-		!commonDataTable.HasColumn(qianjiAppTransactionRelatedIdColumnName) {
-		log.Errorf(ctx, "[qianji_app_transaction_data_csv_file_importer.ParseImportedData] cannot parse import data, because missing essential columns in header row")
-		return nil, nil, nil, nil, nil, nil, errs.ErrMissingRequiredFieldInHeaderRow
-	}
-
-	transactionDataTable, err := c.createNewqianjiAppTransactionDataTable(ctx, commonDataTable)
-
-	if err != nil {
-		return nil, nil, nil, nil, nil, nil, err
-	}
-
-	dataTableImporter := converter.CreateNewSimpleImporterWithTypeNameMapping(qianjiTransactionTypeNameMapping)
-
-	return dataTableImporter.ParseImportedData(ctx, user, transactionDataTable, defaultTimezoneOffset, additionalOptions, accountMap, expenseCategoryMap, incomeCategoryMap, transferCategoryMap, tagMap)
-}
-
-func (c *qianjiAppTransactionDataCsvFileImporter) createNewqianjiAppTransactionDataTable(ctx core.Context, commonDataTable datatable.CommonDataTable) (datatable.TransactionDataTable, error) {
-	newColumns := make([]datatable.TransactionDataTableColumn, 0, 8)
-	newColumns = append(newColumns, datatable.TRANSACTION_DATA_TABLE_TRANSACTION_TYPE)
-	newColumns = append(newColumns, datatable.TRANSACTION_DATA_TABLE_TRANSACTION_TIME)
-
-	if commonDataTable.HasColumn(qianjiAppTransactionCategoryColumnName) {
-		newColumns = append(newColumns, datatable.TRANSACTION_DATA_TABLE_CATEGORY)
-	}
-
-	newColumns = append(newColumns, datatable.TRANSACTION_DATA_TABLE_SUB_CATEGORY)
-	newColumns = append(newColumns, datatable.TRANSACTION_DATA_TABLE_ACCOUNT_NAME)
-
-	if commonDataTable.HasColumn(qianjiAppTransactionAccountCurrencyColumnName) {
-		newColumns = append(newColumns, datatable.TRANSACTION_DATA_TABLE_ACCOUNT_CURRENCY)
-	}
-
-	newColumns = append(newColumns, datatable.TRANSACTION_DATA_TABLE_AMOUNT)
-	newColumns = append(newColumns, datatable.TRANSACTION_DATA_TABLE_RELATED_ACCOUNT_NAME)
-
-	if commonDataTable.HasColumn(qianjiAppTransactionAccountCurrencyColumnName) {
-		newColumns = append(newColumns, datatable.TRANSACTION_DATA_TABLE_RELATED_ACCOUNT_CURRENCY)
-	}
-
-	newColumns = append(newColumns, datatable.TRANSACTION_DATA_TABLE_RELATED_AMOUNT)
-
-	if commonDataTable.HasColumn(qianjiAppTransactionDescriptionColumnName) {
-		newColumns = append(newColumns, datatable.TRANSACTION_DATA_TABLE_DESCRIPTION)
 	}
 
 	transactionRowParser := createQianjiTransactionDataRowParser()
-	transactionDataTable := datatable.CreateNewWritableTransactionDataTableWithRowParser(newColumns, transactionRowParser)
-	transferTransactionsMap := make(map[string]map[datatable.TransactionDataTableColumn]string, 0)
+	transactionDataTable := datatable.CreateNewTransactionDataTableFromBasicDataTableWithRowParser(dataTable, qianjiAppDataColumnNameMapping, transactionRowParser)
+	dataTableImporter := converter.CreateNewSimpleImporterWithTypeNameMapping(qianjiTransactionTypeNameMapping)
 
-	commonDataTableIterator := commonDataTable.DataRowIterator()
-
-	for commonDataTableIterator.HasNext() {
-		dataRow := commonDataTableIterator.Next()
-		rowId := commonDataTableIterator.CurrentRowId()
-
-		if dataRow.ColumnCount() < commonDataTable.HeaderColumnCount() {
-			log.Errorf(ctx, "[qianji_app_transaction_data_csv_file_importer.createNewqianjiAppTransactionDataTable] cannot parse row \"%s\", because may missing some columns (column count %d in data row is less than header column count %d)", rowId, dataRow.ColumnCount(), commonDataTable.HeaderColumnCount())
-			return nil, errs.ErrFewerFieldsInDataRowThanInHeaderRow
-		}
-
-		data := make(map[datatable.TransactionDataTableColumn]string, 8)
-
-		for columnType, columnName := range qianjiAppDataColumnNameMapping {
-			if dataRow.HasData(columnName) {
-				data[columnType] = dataRow.GetData(columnName)
-			}
-		}
-
-		transactionType := data[datatable.TRANSACTION_DATA_TABLE_TRANSACTION_TYPE]
-
-		if transactionType == qianjiAppTransactionTypeIncomeText || transactionType == qianjiAppTransactionTypeExpenseText {
-			switch transactionType {
-			case qianjiAppTransactionTypeIncomeText:
-				data[datatable.TRANSACTION_DATA_TABLE_TRANSACTION_TYPE] = qianjiTransactionTypeNameMapping[models.TRANSACTION_TYPE_INCOME]
-			case qianjiAppTransactionTypeExpenseText:
-				data[datatable.TRANSACTION_DATA_TABLE_TRANSACTION_TYPE] = qianjiTransactionTypeNameMapping[models.TRANSACTION_TYPE_EXPENSE]
-			}
-
-			transactionDataTable.Add(data)
-		} else {
-			log.Errorf(ctx, "[qianji_app_transaction_data_csv_file_importer.createNewqianjiAppTransactionDataTable] cannot parse transaction type \"%s\" in row \"%s\"", transactionType, rowId)
-			return nil, errs.ErrTransactionTypeInvalid
-		}
-	}
-
-	if len(transferTransactionsMap) > 0 {
-		log.Errorf(ctx, "[qianji_app_transaction_data_csv_file_importer.createNewqianjiAppTransactionDataTable] there are %d transactions (related id is %s) which don't have related records", len(transferTransactionsMap), c.getqianjiAppRelatedTransactionIds(transferTransactionsMap))
-		return nil, errs.ErrFoundRecordNotHasRelatedRecord
-	}
-
-	return transactionDataTable, nil
-}
-
-func (c *qianjiAppTransactionDataCsvFileImporter) getqianjiAppRelatedTransactionIds(transferTransactionsMap map[string]map[datatable.TransactionDataTableColumn]string) string {
-	builder := strings.Builder{}
-
-	for relatedId := range transferTransactionsMap {
-		if builder.Len() > 0 {
-			builder.WriteRune(',')
-		}
-
-		builder.WriteString(relatedId)
-	}
-
-	return builder.String()
+	return dataTableImporter.ParseImportedData(ctx, user, transactionDataTable, defaultTimezoneOffset, additionalOptions, accountMap, expenseCategoryMap, incomeCategoryMap, transferCategoryMap, tagMap)
 }
