799d559c55f770ba5ae978b2c8cc067b9f8d9ce9
fireworld <fuxudong103@gmail.com>
2025-12-14 11:10:39 +0800

    同步更新数据

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -9,6 +9,7 @@ import ko from './ko.json';
 import nl from './nl.json';
 import ru from './ru.json';
 import th from './th.json';
+import tr from './tr.json';
 import uk from './uk.json';
 import vi from './vi.json';
 import zhHans from './zh_Hans.json';
@@ -117,6 +118,13 @@ export const ALL_LANGUAGES: Record<string, LanguageInfo> = {
         alternativeLanguageTag: 'th-TH',
         textDirection: 'ltr',
         content: th
+    },
+    'tr': {
+        name: 'Turkish',
+        displayName: 'Türkçe',
+        alternativeLanguageTag: 'tr-TR',
+        textDirection: 'ltr',
+        content: tr
     },
     'uk': {
         name: 'Ukrainian',
